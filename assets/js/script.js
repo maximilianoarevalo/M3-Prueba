5ce24d68ead3990df7a3611d383b872bcb45ac0b
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2024-02-16 23:07:45 -0300

    Obtener conversion funcional

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,78 @@ async function fetchDataFromAPI(){
     }
 }
 
-async function getAllCurrencies(){
-    const allData = await fetchDataFromAPI();
-    const select = document.getElementById('selector');
-    
-    console.log(allData) // quitar
-    for(element in allData){
-        if(typeof allData[element] === 'object'){
-            console.log(allData[element]['nombre']) // quitar
-            select.innerHTML += `<option>${allData[element]['nombre']}</option>`
-        }
+// async function fetchIndicatorsFromAPI(){
+//     let indicatorsObject = [];
+//     try{
+//         const res = await fetch("https://mindicador.cl/api/").then(function(response){
+//             let indicator = {};
+//             indicator.nombre = response.uf.nombre;
+//             indicator.valor = response.uf.valor;
+//             indicatorsObject.push(indicator)
+//         });
+//     }catch(e){
+//         alert(e.message);
+//     }
+//     console.log(indicatorsObject)
+// }
+
+// fetchIndicatorsFromAPI();
+
+async function getCurrency(currency){
+    try{
+        const res = await fetch(`https://mindicador.cl/api/${currency}`);
+        const data = await res.json();
+        return data
+    }catch(e){
+        alert(e.message);
     }
-    
 }
 
-getAllCurrencies();
+async function clpValueToCurrency(){
+    try{
+        let clpValue = document.getElementById('clp-value').value;
+        console.log(clpValue)
+        let selectedCurrency = document.getElementById('selector').options[selector.selectedIndex].value;
+        console.log(selectedCurrency)
+        let currencyArray = await getCurrency(selectedCurrency);
+        console.log(currencyArray.serie[0])
+        let actualValueOfCurrency = currencyArray.serie[0].valor;
+        console.log(selectedCurrency)
+        
+        // Switch para determinar que hacer
+        switch(selectedCurrency){
+            case 'uf':
+                result = (parseInt(clpValue)/actualValueOfCurrency).toFixed(4);
+                break;
+            case 'bitcoin':
+                result = (parseInt(clpValue)/actualValueOfCurrency).toFixed(4);
+                break;
+            case 'euro':
+                result = (actualValueOfCurrency * parseInt(clpValue)).toFixed(4);
+                break;
+            case 'dolar':
+                result = (actualValueOfCurrency * parseInt(clpValue)).toFixed(4);
+                break;
+            case 'dolar_intercambio':
+                result = (actualValueOfCurrency * parseInt(clpValue)).toFixed(4);
+                break;
+        }
+        
+        // Se revisa el valor de result, si los decimales son solo 0s se eliminan
+        let decimals = result.toString().split('.')
+        console.log(decimals)
+        if(parseInt(decimals[1]) == 0){
+            result = decimals[0];
+        }
+
+        // Se coloca el resultado en el DOM
+        let domValue = document.getElementById('money-result');
+        domValue.innerHTML= result;
+
+        console.log(result)
+        
+    } catch(e){
+        alert(e.message)
+    }
+// Obtener la opción seleccionada
+}
