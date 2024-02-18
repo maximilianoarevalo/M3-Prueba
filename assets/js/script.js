261543881795fae4d4d2ec46417218ca5d57bccb
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2024-02-17 21:54:58 -0300

    Desafio terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Funcion que obtiene la data de la divisa seleccionada desde la API
 async function getCurrency(currency) {
   try {
     const res = await fetch(`https://mindicador.cl/api/${currency}`);
@@ -10,25 +11,27 @@ async function getCurrency(currency) {
   }
 }
 
+// Funcion que se gatilla desde el DOM
 async function clpValueToCurrency() {
   try {
+
+    // Se verifica si el campo de pesos tiene valor y que sea numerico
     let clpValue = document.getElementById("clp-value").value;
     if(clpValue == null || clpValue == undefined || isNaN(parseInt(clpValue))){
-      alert('Por favor, ingrese un número válido.')
+      alert('Por favor, ingrese un número válido.');
       return
     }
-    console.log(clpValue);
+
+    // Se obtiene el valor de la divisa a convertir seleccionada
     let selectedCurrency = document.getElementById("selector").options[selector.selectedIndex].value;
-    console.log(selectedCurrency);
+    // Se almacena la respuesta y se asigna el valor de la divisa
     let currencyArray = await getCurrency(selectedCurrency);
-    console.log(currencyArray.serie[0]);
     let actualValueOfCurrency = currencyArray.serie[0].valor;
-    console.log(selectedCurrency);
 
     // Se calcula el cambio
     result = (parseInt(clpValue) / actualValueOfCurrency).toFixed(4);
 
-    // Se revisa el valor de result, si los decimales son solo 0s se eliminan
+    // Se revisa el valor de result, si los decimales son solo 0s se eliminan (para que no quede el valor X.000)
     let decimals = result.toString().split(".");
     console.log(decimals);
     if (parseInt(decimals[1]) == 0) {
@@ -38,7 +41,7 @@ async function clpValueToCurrency() {
     // Se coloca el resultado en el DOM
     let domValue = document.getElementById("money-result");
 
-    // Se coloca el signo correcto para cada tipo de cambio
+    // Se coloca el signo correcto para cada tipo de cambio (esto agrega al DOM el UF, $ o Euro segun corresponda)
     if (selectedCurrency == "uf") {
       domValue.innerHTML = `Resultado: ${result} UF`;
     } else if (selectedCurrency == "euro") {
@@ -47,30 +50,31 @@ async function clpValueToCurrency() {
       domValue.innerHTML = `Resultado: $ ${result} `;
     }
 
-    //Se llama la render del grafico
-    let lastDaysValuesArray = currencyArray.serie.slice(1, 10);
-    renderGrafica(lastDaysValuesArray.reverse());
+    //Se llama la render del grafico con la data de los ultimos 10 dias
+    let lastDaysValuesArray = currencyArray.serie.slice(0, 10);
+    renderGraph(lastDaysValuesArray.reverse());
 
-    console.log(result);
   } catch (e) {
     // Se escribe error en el DOM para el catch
     let domValue = document.getElementById("money-result");
     domValue.innerHTML = `Algo salió mal! Error: ${e.message}`;
   }
-  // Obtener la opción seleccionada
 }
 
-async function getAndCreateDataToChart(currencyArray) {
-  console.log(currencyArray);
+// Funcion que parsea la data de la divisa para dibujar en el grafico
+async function getDataToChart(currencyArray) {
+  // Se obtienen las fechas del array de valores de la divisa correspondiente
   const labels = currencyArray.map((currency) => {
     return currency.fecha.split('T')[0];
   });
-  
+
+  // Se obtiene el array de valores de la divisa correspondiente a los ultimos 10 dias
   const data = currencyArray.map((currency) => {
     const valor = currency.valor;
     return valor;
   });
 
+  // Se define el dataset
   const datasets = [
     {
       label: "Historial últimos 10 días",
@@ -81,22 +85,25 @@ async function getAndCreateDataToChart(currencyArray) {
   return { labels, datasets };
 }
 
-async function renderGrafica(lastDaysValuesArray) {
-    //Se elimina un chat previo
+// Funcion que agrega el grafico al DOM
+async function renderGraph(lastDaysValuesArray) {
+    //Se elimina el grafico anterior (si lo hubiese)
     const myChartElement = document.getElementById("myChart");
-
     const existingChart = Chart.getChart(myChartElement);
 
-    // Destruir el gráfico existente si hay uno
+    // Destruir el gráfico existente (si hay uno, esto para evitar un error de sobreescribir el chart)
     if (existingChart) {
         existingChart.destroy();
     }
 
-    const data = await getAndCreateDataToChart(lastDaysValuesArray);
+    // Se obtiene la data del char y se define su configuracion
+    const data = await getDataToChart(lastDaysValuesArray);
     const config = {
         type: "line",
         data,
     };
+
+    // Se agrega el chart al DOM
     const myChart = document.getElementById("myChart");
     myChart.style.backgroundColor = "white";
     new Chart(myChart, config);
